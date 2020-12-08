a1d32f3b91028384ead19a9171db630192d033d7
achu072 <68572833+achu072@users.noreply.github.com>
2020-12-09 12:22:47 +1300

    Added Atmel and Proteus projects for testing

---
+++ PrimaryConverter/Firmware/Atmel/2020_srw_rc_car_primary/2020_srw_rc_car_primary/timer0.h
@@ -0,0 +1,16 @@
+/*
+ * timer0.h
+ *
+ * Created: 9/12/2020 12:03:00 pm
+ *  Author: achu072
+ */ 
+
+
+#ifndef TIMER0_H_
+#define TIMER0_H_
+
+void timer0_init();
+
+
+
+#endif /* TIMER0_H_ */
---
+++ PrimaryConverter/Firmware/Atmel/2020_srw_rc_car_primary/2020_srw_rc_car_primary/timer2.h
@@ -0,0 +1,16 @@
+/*
+ * timer2.h
+ *
+ * Created: 9/12/2020 12:03:28 pm
+ *  Author: achu072
+ */ 
+
+
+#ifndef TIMER2_H_
+#define TIMER2_H_
+
+
+
+
+
+#endif /* TIMER2_H_ */
