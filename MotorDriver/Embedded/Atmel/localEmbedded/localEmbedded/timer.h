66e334c781d966f5a7d92d353f7171186d3a9ce4
Esta201 <esta201@aucklanduni.ac.nz>
2020-12-16 11:13:31 +1300

    more PWMs

---
+++ MotorDriver/Embedded/Atmel/localEmbedded/localEmbedded/timer.h
@@ -0,0 +1,18 @@
+/*
+ * timer.h
+ *
+ * Created: 9/12/2020 2:54:34 pm
+ *  Author: Earlene
+ */ 
+
+
+#ifndef TIMER_H_
+#define TIMER_H_
+#include <stdbool.h>
+#include <stdint.h>
+#include <avr/interrupt.h>
+
+timer0_init();
+timer2_init();
+
+#endif /* TIMER_H_ */
