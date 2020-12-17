5bf03367241a1c1bf3f0a1fe7cac9ac3179926bb
mkmu260 <68840505+mkmu260@users.noreply.github.com>
2020-12-17 14:48:11 +1300

    Completed ADC

--- SecondaryConverter/Firmware/PWM_Generation/PWM_Generation/adc.c
+++ SecondaryConverter/Firmware/PWM_Generation/PWM_Generation/adc.c
@@ -5,6 +5,25 @@
  *  Author: Manoj
  */ 
 
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#define REF_VOLTAGE 12
+#define RESOLUTION 1024
+
+static volatile int16_t data_array[100];
+static volatile uint8_t count = 0; 
+
+ISR(ADC_vect) {
+	int16_t data = ADC;
+	data_array[count] = (int32_t)data * REF_VOLTAGE/RESOLUTION;
+	count++;
+	
+}
+
 void adc_init() {
-	ADMUX |= (1<<REFS0);
+	ADMUX = 0b01000000;
+	ADCSRA |= (1<<ADEN);
+	ADCSRA |= (1<<ADSC);
+	ADCSRA |= (1<<ADATE);
+	ADCSRA |= (1<<ADIE);
 }
