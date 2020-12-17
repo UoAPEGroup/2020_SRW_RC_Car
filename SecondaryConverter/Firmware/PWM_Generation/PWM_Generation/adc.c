22c4ecb20a2ef227a22fb3b8f69cd92fa84f6c62
uoa-pgul065 <pgul065@aucklanduni.ac.nz>
2020-12-17 15:00:10 +1300

    Added ADC header file

--- SecondaryConverter/Firmware/PWM_Generation/PWM_Generation/adc.c
+++ SecondaryConverter/Firmware/PWM_Generation/PWM_Generation/adc.c
@@ -7,6 +7,9 @@
 
 #include <avr/io.h>
 #include <avr/interrupt.h>
+#include "adc.h"
+
+
 #define REF_VOLTAGE 12
 #define RESOLUTION 1024
 
@@ -14,6 +17,7 @@ static volatile int16_t data_array[100];
 static volatile uint8_t count = 0; 
 
 ISR(ADC_vect) {
+	
 	int16_t data = ADC;
 	data_array[count] = (int32_t)data * REF_VOLTAGE/RESOLUTION;
 	count++;
