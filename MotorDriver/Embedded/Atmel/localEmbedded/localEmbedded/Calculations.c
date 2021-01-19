d59288b04e33030b90f8507f028f5d6205d49b5b
rainingx683 <68803766+rainingx683@users.noreply.github.com>
2021-01-19 17:09:50 +1300

    Calculation update

--- MotorDriver/Embedded/Atmel/localEmbedded/localEmbedded/Calculations.c
+++ MotorDriver/Embedded/Atmel/localEmbedded/localEmbedded/Calculations.c
@@ -8,19 +8,19 @@
 #include <stdint.h>
 #include <stdbool.h>
 
-#define PERIODHALF 26
+#define PERIODHALF 26 //half of the counts for the period
 #define TOLERANCE 500
 #define ARRAY_SIZE 10
 
 //declare variables
-static volatile uint8_t dutyCycleNum = 0; //on time of the wave through the motor
-static volatile uint8_t dutyLHalf = 0; //on time of the left mosfets
-static volatile uint8_t dutyRHalf = 0; //on time of the right mosfets
+static volatile uint8_t finalOnTime = 0; //on time of the wave through the motor(in number of counts)
+static volatile uint8_t leftOnTime = 0; //on time of the left mosfets(in number of counts)
+static volatile uint8_t rightOnTime = 0; //on time of the right mosfets(in number of counts)
 
-static volatile uint16_t dutyCycleVoltage = 0; //input voltage to the H-bridge
-static volatile uint16_t newVoltage = 0; //most recent voltage reading into the H-bridge
-static volatile uint16_t Vout = 0; //voltage wanted across motor, set with setSpeedGrade
-static volatile bool forward = true; //determines whether the car is moving forward or backward
+static volatile uint16_t inputV = 0; //input voltage to the H-bridge
+static volatile uint16_t recentInputV = 0; //most recent voltage reading into the H-bridge
+static volatile uint16_t speedGrade = 0; //voltage wanted across motor, set with setSpeedGrade
+static volatile uint8_t forward = 1; //determines whether the car is moving forward or backward
 
 //create arrays
 static volatile uint16_t voltageValues[ARRAY_SIZE];
@@ -47,44 +47,43 @@ void addVoltage(uint16_t adcVoltageReading) {
 	}
 }
 
-
-uint8_t returnDutyLHalf(){
-	return dutyLHalf;	
+void updateDutyCycle(){
+	
+		//if the input voltage to the H-bridge is greater than the voltage wanted across the motor:
+		if(inputV > speedGrade){
+			
+			finalOnTime = (PERIODHALF*speedGrade)/inputV; //calculate the on time of the final wave across the motor
+			
+			if (forward == 1){
+				leftOnTime = (PERIODHALF/2) + (finalOnTime/2); //set the on time of left fets
+				rightOnTime = (PERIODHALF/2) - (finalOnTime/2); //set the on time of the right fets
+				}else{
+				leftOnTime = (PERIODHALF/2) - (finalOnTime/2); //set on time of the left fets
+				rightOnTime = (PERIODHALF/2) + (finalOnTime/2); //sset the on time of the right fets
+				}
+			
+		}else{
+				//set the duty cycle to maximum
+				leftOnTime = PERIODHALF - 1;
+				rightOnTime = 1;
+			}
+}
+void setInputV(uint16_t vinD) {
+	inputV = vinD;
 }
 
-uint8_t returnDutyRHalf(){
-	return dutyRHalf;
+void setSpeedGrade(uint16_t speed){
+	speedGrade = speed;
+}
+
+uint8_t returnLeftOnTime(){
+	return leftOnTime;
+}
+
+uint8_t returnRightOnTime(){
+	return rightOnTime;
 }
 
 uint8_t returnPeriodHalf(){
 	return PERIODHALF;
-}
-
-void updateDutyCycle(){
-	
-		//if the input voltage to the H-bridge is greater than the voltage wanted across the motor:
-		if(dutyCycleVoltage > Vout){
-			
-			dutyCycleNum = (PERIODHALF*Vout)/dutyCycleVoltage; //calculate the on time of the final wave across the motor
-			
-			if (forward){
-				dutyLHalf = (PERIODHALF/2) + (dutyCycleNum/2); //set the on time of left fets
-				dutyRHalf = (PERIODHALF/2) - (dutyCycleNum/2); //set the on time of the right fets
-				}else{
-				dutyLHalf = (PERIODHALF/2) - (dutyCycleNum/2); //set on time of the left fets
-				dutyRHalf = (PERIODHALF/2) + (dutyCycleNum/2); //sset the on time of the right fets
-			}
-			
-		}else{
-				//set the duty cycle to maximum
-				dutyLHalf = PERIODHALF - 1;
-				dutyRHalf = 1;
-			}
-}
-void setDutyCycleVoltage(uint16_t vinD) {
-	dutyCycleVoltage = vinD;
-}
-
-void setSpeedGrade(uint16_t speed){
-	Vout = speed;
 }
--- MotorDriver/Embedded/Atmel/localEmbedded/localEmbedded/Calculations.h
+++ MotorDriver/Embedded/Atmel/localEmbedded/localEmbedded/Calculations.h
@@ -10,13 +10,13 @@
 #define CALCULATIONS_H_
 
 //variables
-static volatile uint8_t duty_cycle;
-static volatile uint8_t dutyLHalf;
-static volatile uint8_t dutyRHalf;
+static volatile uint8_t finalOnTime;
+static volatile uint8_t leftOnTime;
+static volatile uint8_t rightOnTime;
 
-static volatile uint16_t dutyCycleVoltage;
-static volatile uint16_t newVoltage;
-static volatile uint16_t Vout;
+static volatile uint16_t inputV;
+static volatile uint16_t recentInputV;
+static volatile uint16_t speedGrade;
 
 static volatile uint16_t voltageValues;
 static volatile uint16_t currentValues;
@@ -27,11 +27,11 @@ static volatile uint8_t count = 0;
 extern volatile bool arrayFull;
 
 //functions
-uint8_t returnDutyLHalf();
-uint8_t returnDutyRHalf();
+uint8_t returnLeftOnTime();
+uint8_t returnRightOnTime();
 uint8_t returnPeriodHalf();
 void updateDutyCycle();
-void setDutyCycleVoltage(uint16_t vinD);
+void setInputV(uint16_t vinD);
 void setSpeedGrade(uint16_t speed);
 void addCurrent(uint16_t adcCurrentReading);
 void addVoltage(uint16_t adcVoltageReading);
