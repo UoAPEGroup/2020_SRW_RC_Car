d414741b23a0d9baacfc72b901cb1e0221244686
achu072 <68572833+achu072@users.noreply.github.com>
2021-01-28 10:08:03 +1300

    Tidy up of comments.....OCD triggered

--- PrimaryConverter/Firmware/Atmel_final/2020_srw_rc_car_primary/adc.c
+++ PrimaryConverter/Firmware/Atmel_final/2020_srw_rc_car_primary/adc.c
@@ -18,15 +18,15 @@ volatile uint8_t temp2_counter = 0;
 volatile uint8_t temp3_counter = 0;
 
 //ADC transfer booleans
-bool adc_full = false; //set all booleans to false 
+bool adc_full = false;														//set all booleans to false 
 
 //ADC read arrays
-uint32_t adc_voltage[SAMPLING_SIZE]; //mV
-uint32_t adc_current[SAMPLING_SIZE]; //mA
-uint32_t adc_temp1[SAMPLING_SIZE]; //mV
-uint32_t adc_temp2[SAMPLING_SIZE]; //mV
-uint32_t adc_temp3[SAMPLING_SIZE]; //mV
-uint32_t adc_averages[ADC_used] = {0}; // voltage[0], current[1], temp1[2], temp2[3], temp3[4]
+uint32_t adc_voltage[SAMPLING_SIZE];										//mV
+uint32_t adc_current[SAMPLING_SIZE];										//mA
+uint32_t adc_temp1[SAMPLING_SIZE];											//mV
+uint32_t adc_temp2[SAMPLING_SIZE];											//mV
+uint32_t adc_temp3[SAMPLING_SIZE];											//mV
+uint32_t adc_averages[ADC_used] = {0};										//voltage[0], current[1], temp1[2], temp2[3], temp3[4]
 
 ISR(ADC_vect) {
 	if (adc_full == false) {
@@ -47,7 +47,7 @@ ISR(ADC_vect) {
 		}
 		else if (current_counter < SAMPLING_SIZE) {
 			TIMER1_COMPB_CLR;
-			adc_current[current_counter] = adc_convert_mV(ADC); //conversion for mA, needs adjustment
+			adc_current[current_counter] = adc_convert_mV(ADC);				//conversion for mA, needs adjustment
 			//check for rated value and cut
 			/*if (adc_voltage[voltage_counter] >= rated_C) {
 				reset_counters(); 
@@ -62,7 +62,7 @@ ISR(ADC_vect) {
 		}
 		else if (temp1_counter < SAMPLING_SIZE) {
 			TIMER1_COMPB_CLR;
-			adc_temp1[temp1_counter] = adc_convert_mV(ADC); //conversion for temp different, needs adjustment
+			adc_temp1[temp1_counter] = adc_convert_mV(ADC);					//conversion for temp different, needs adjustment
 			//check for rated value and cut
 			/*if (adc_voltage[voltage_counter] >= rated_T1) {
 				reset_counters(); 
@@ -77,7 +77,7 @@ ISR(ADC_vect) {
 		}
 		else if (temp2_counter < SAMPLING_SIZE) {
 			TIMER1_COMPB_CLR;
-			adc_temp2[temp2_counter] = adc_convert_mV(ADC); //conversion for temp different, needs adjustment
+			adc_temp2[temp2_counter] = adc_convert_mV(ADC);					//conversion for temp different, needs adjustment
 			//check for rated value and cut
 			/*if (adc_voltage[voltage_counter] >= rated_T2) {
 				reset_counters(); 
@@ -92,7 +92,7 @@ ISR(ADC_vect) {
 		}
 		else if (temp3_counter < SAMPLING_SIZE) {
 			TIMER1_COMPB_CLR;
-			adc_temp3[temp3_counter] = adc_convert_mV(ADC); //conversion for temp different, needs adjustment
+			adc_temp3[temp3_counter] = adc_convert_mV(ADC);					//conversion for temp different, needs adjustment
 			//check for rated value and cut
 			/*if (adc_voltage[voltage_counter] >= rated_T3) {
 				reset_counters(); 
@@ -116,27 +116,25 @@ ISR(ADC_vect) {
 }
 
 void adc_init() {
-	ADMUX |= (1<<REFS0); //Reference voltage selected to be AVCC
-	
-	ADCSRA |= (1<<ADEN); //ADC enabled
-	ADC_CH_VSENS;		//set start channel to vsens
-	ADCSRA |= (1<<ADATE); //ADC auto trigger enabled
-	ADCSRA |= (1<<ADIE); //ADC interrupt enabled
-	ADCSRA |= (1<<ADPS2) | (1<<ADPS0); //ADC prescaler = 32, system clock = 8Mhz, adc clock = 250kHz
-	
-	ADCSRB |= (1<<ADTS2) | (1<<ADTS0); //ADC auto trigger source = Timer/Counter1 compare match B
+	ADMUX |= (1<<REFS0);													//Reference voltage selected to be AVCC
+	ADCSRA |= (1<<ADEN);													//ADC enabled
+	ADC_CH_VSENS;															//set start channel to vsens
+	ADCSRA |= (1<<ADATE);													//ADC auto trigger enabled
+	ADCSRA |= (1<<ADIE);													//ADC interrupt enabled
+	ADCSRA |= (1<<ADPS2) | (1<<ADPS0);										//ADC prescaler = 32, system clock = 8Mhz, adc clock = 250kHz
+	ADCSRB |= (1<<ADTS2) | (1<<ADTS0);										//ADC auto trigger source = Timer/Counter1 compare match B
 }
 
 uint16_t adc_read(uint8_t channel) {
-	ADMUX &= ~( (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0) ); //clearing MUX register
-	ADMUX |= channel; //select the adc channel
+	ADMUX &= ~( (1<<MUX3) | (1<<MUX2) | (1<<MUX1) | (1<<MUX0) );			//clearing MUX register
+	ADMUX |= channel;														//select the adc channel
 	
-	return ADC; //return the ADC value from the ADCL and ADCH registers
+	return ADC;																//return the ADC value from the ADCL and ADCH registers
 }
 
 //convert the raw adc value to original value by multiplying with Vstep in mV
 uint32_t adc_convert_mV(uint16_t raw_ADC) {  
-	uint32_t original_V = raw_ADC*V_ref;  //reference voltage, amplitude interval
+	uint32_t original_V = raw_ADC*V_ref;									//reference voltage, amplitude interval
 	original_V = original_V/A_int;
 	return original_V;							
 }
